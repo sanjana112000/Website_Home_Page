b9cbba989e992eb831e97fc4f1f19618c6e7f3a1
Sanjana-Raj <61341209+sanjana112000@users.noreply.github.com>
2023-04-26 13:41:09 +0000

    Pending changes exported from your codespace

---
+++ src/components/Glob5.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import image11 from '../images/Group 35.png'
+
+
+function Glob5() {
+   
+    return (
+       <div className='container6'>
+
+            <div className='gb gb1'>
+                <div id='gbb1'>
+                 <h2 >AJMAN OFFSHORE</h2>   
+                 <h1> <span ><b>$2,997</b> </span><p id='p4'>One Time Payment</p></h1>
+                    <p id=''><b>Ajman Offshore New Company formation includes</b></p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Registered Agent</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Registered Office</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Varal Administrative</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Preparing Statutory File</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Liasing with Registration Authorities</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Due Diligence Appraisal</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Keeping The Register</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Seal Charges</p>   
+                    <p id=''><img id='img11'src={image11} alt='' /> Certificate of Good Standing</p>    
+                    <input id='a3' type='submit' value='Continue' />      
+                </div>
+                
+
+            </div>
+            <div className='gb gb2' >
+                <div id='gbb2'>
+                 <h2>RAK ICC</h2>   
+                 <h1> <span ><b>$2,997</b> </span><p id='p4'>One Time Payment</p></h1>
+                    <p id=''><b>Rak Icc New Company information  includes</b></p>
+                   
+                    <p id=''><img id='img11'src={image11} alt='' /> Registered Agent</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Registered Office</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Varal Administrative</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Preparing Statutory File</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Liasing with Registration Authorities</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Due Diligence Appraisal</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Keeping The Register</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Seal Charges</p>   
+                    <p id=''><img id='img11'src={image11} alt='' /> Certificate of Good Standing</p>    
+                    <input id='a3' type='submit' value='Continue' />      
+                </div>
+            </div>
+
+            <div className='gb gb3'>
+                <div id='gbb3'>
+                 <h2>SHARJAH MEDIA CITY</h2>   
+                 <h1> <span id=''><b>$4,124</b> </span><p id='p4'>One Time Payment</p></h1>
+                    <p id=''><b>Sharjah Media City New<br/>
+                            Company formation include</b></p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Registered Agent</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Registered Office</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Varal Administrative</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Preparing Statutory File</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Liasing with Registration Authorities</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Due Diligence Appraisal</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Keeping The Register</p>
+                    <p id=''><img id='img11'src={image11} alt='' /> Seal Charges</p>   
+                    <p id=''><img id='img11'src={image11} alt='' /> Certificate of Good Standing</p>    
+                    <input id='a3' type='submit' value='Continue' />      
+                </div>
+
+
+            </div>
+        </div>
+  
+    );
+}
+
+export default Glob5;
